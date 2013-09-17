506fb3b2eaac9e9de6ee2eb9b8cf379f7ef34de3
hongqing.chen <hongqing.chen@gmail.com>
2013-09-17 21:47:16 +0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OSUPortal::Application.config.secret_token = '43e2258b7b7495643ae378cbf3b5891ee4a715e14106027e0c260e52a1a115cc82cfc8b53e06535066a6c4aa1f42aa08bfa99c001136b08d820e50785839cf5b'
