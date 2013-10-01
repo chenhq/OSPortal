27bbc2d15307dc4541bf4d6c0e34f40f3ca7d5f7
hongqing.chen <hongqing.chen@gmail.com>
2013-10-01 11:21:59 +0800

    finish instance

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,99 @@
+require 'openstack_activeresource'
+class ImagesController < ApplicationController
+  # GET /images
+  # GET /images.json
+  def auth
+    # Set Keystone Public API endpoint
+    OpenStack::Keystone::Public::Base.site = "http://122.227.254.55:5000/v2.0/"
+
+    # Authentication
+    auth = OpenStack::Keystone::Public::Auth.create :username => "admin", :password => "175245a126f74b02", :tenant_id => "e486e554e533455b83389720826d4c80"
+
+    # Set the auth token for next API requests
+    OpenStack::Base.token = auth.token
+
+    # Set the Nova Compute API endpoint from the received service catalog
+    OpenStack::Nova::Compute::Base.site = auth.endpoint_for('compute').publicURL
+  end
+
+
+  def index
+    auth
+    @images = OpenStack::Nova::Compute::Image.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @images }
+    end
+  end
+
+  # GET /images/1
+  # GET /images/1.json
+  def show
+    @image = Image.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @image }
+    end
+  end
+
+  # GET /images/new
+  # GET /images/new.json
+  def new
+    @image = Image.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @image }
+    end
+  end
+
+  # GET /images/1/edit
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  # POST /images
+  # POST /images.json
+  def create
+    @image = Image.new(params[:image])
+
+    respond_to do |format|
+      if @image.save
+        format.html { redirect_to @image, notice: 'Image was successfully created.' }
+        format.json { render json: @image, status: :created, location: @image }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /images/1
+  # PUT /images/1.json
+  def update
+    @image = Image.find(params[:id])
+
+    respond_to do |format|
+      if @image.update_attributes(params[:image])
+        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /images/1
+  # DELETE /images/1.json
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+
+    respond_to do |format|
+      format.html { redirect_to images_url }
+      format.json { head :no_content }
+    end
+  end
+end
